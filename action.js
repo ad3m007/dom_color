f0bcbc8133e1a33f08c5ef0e2005c75debc43669
adam <essidadem2005@gmail.com>
2025-05-26 01:11:27 +0100

    first commit

---
+++ action.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var box = document.querySelector("#color-box");
+  var btn = document.querySelector("#change-color-btn");
+
+  btn.addEventListener("click", function () {
+    var x = getRandomColor();
+    box.style.backgroundColor = x;
+  });       
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let colorx = '#';
+  for (let i = 0; i < 6; i++) {
+    colorx += letters[Math.floor(Math.random() * 16)];
+  }
+  return colorx;
+}
